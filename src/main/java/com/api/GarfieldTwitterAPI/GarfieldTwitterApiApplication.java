557444144c372cced26b45d8a44d669250c7a911
brandon-ttr <cutehippo116@gmail.com>
2022-07-04 14:25:53 -0400

    Boilerplate

---
+++ src/main/java/com/api/GarfieldTwitterAPI/GarfieldTwitterApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.GarfieldTwitterAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarfieldTwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarfieldTwitterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/GarfieldTwitterAPI/GarfieldTwitterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.GarfieldTwitterAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarfieldTwitterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
